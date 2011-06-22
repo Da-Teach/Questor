03fdb64072a4208d1e0b41cf56a4f0a29826530e
Da-Teach <da_teach@thehackerwithin.com>
2011-06-22 19:35:41 +0200

    Fix for auto-login Fix for Salvagers being in the same group as Tractor Beams

--- Questor.Modules/GroupID.cs
+++ Questor.Modules/GroupID.cs
@@ -65,9 +65,6 @@ namespace Questor.Modules
         SiegeMissileLaunchers = 508,
         CruiseMissileLaunchers = 506,
 
-        Salvager = 538,
-        TractorBeam = 650,
-
         Wreck = 186,
     }
 }
