7ad8c752a0ad5bd7033f6de11c90afadc680ccda
deXtoRious <dextorious@gmail.com>
2011-09-03 19:03:17 +0300

    Compile.bat fix.

--- #Compile#.bat
+++ #Compile#.bat
@@ -2,6 +2,7 @@ del .\questor\bin\debug\*.* /Q
 del .\questor.modules\bin\debug\*.* /Q
 c:\Windows\Microsoft.NET\Framework\v3.5\msbuild
 mkdir output
+del .\output\*.* /Q
 copy .\questor\bin\debug\*.exe .\output\*.exe
 copy .\questor\bin\debug\*.dll .\output\*.dll
 copy .\gotobm\bin\debug\*.exe .\output\*.exe
