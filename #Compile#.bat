74fd76b7a3c3cc5b0142aab1577a097e19449869
deXtoRious <dextorious@gmail.com>
2011-09-03 16:03:10 +0300

    Modified compile.bat for convenience

--- #Compile#.bat
+++ #Compile#.bat
@@ -1,4 +1,10 @@
 del .\questor\bin\debug\*.* /Q
 del .\questor.modules\bin\debug\*.* /Q
 c:\Windows\Microsoft.NET\Framework\v3.5\msbuild
+mkdir output
+copy .\questor\bin\debug\*.exe .\output\*.exe
+copy .\questor\bin\debug\*.dll .\output\*.dll
+copy .\gotobm\bin\debug\*.exe .\output\*.exe
+copy .\updateinvtypes\bin\debug\*.exe .\output\*.exe
+copy .\valuedump\bin\debug\*.exe .\output\*.exe
 pause
