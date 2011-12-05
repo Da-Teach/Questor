702f0f51144ef1861e9ddae89e076bb4e2efad28
Da-Teach <da_teach@thehackerwithin.com>
2011-12-05 15:07:53 +0100

    Scrap Metal is no longer seen as the big-bad-evil (seeing as its now 0.01 volume) ReinforcedMetalScraps will be handled by the normal loot-value-code

--- Questor.Modules/ItemCache.cs
+++ Questor.Modules/ItemCache.cs
@@ -58,11 +58,6 @@ namespace Questor.Modules
             }
         }
 
-        public bool IsScrapMetal
-        {
-            get { return TypeId == 30497 || TypeId == 15331; }
-        }
-
         public string Name
         {
             get { return _directItem.TypeName; }
