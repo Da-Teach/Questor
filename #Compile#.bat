f05590966f76733c46984f8051b5cfac1654e1f4
SystemControl <systemcontrol.c@gmail.com>
2011-12-14 00:05:25 +0100

    New Compile.bat and cleaning
    Add Mission Caldari L4

--- #Compile#.bat
+++ #Compile#.bat
@@ -1,13 +1,23 @@
 del .\questor\bin\debug\*.* /Q
 del .\questor.modules\bin\debug\*.* /Q
-c:\Windows\Microsoft.NET\Framework\v3.5\msbuild
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\gotobm\GoToBM.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\BuyLPI\BuyLPI.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\Traveler\Traveler.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\questor\Questor.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\Questor.Modules\Questor.Modules.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\updateinvtypes\UpdateInvTypes.csproj
+c:\Windows\Microsoft.NET\Framework\v3.5\msbuild .\valuedump\ValueDump.csproj
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild .\questorstatistics\QuestorStatistics.csproj
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild .\QuestorSettings\QuestorSettings.csproj
 mkdir output
 del .\output\*.* /Q
 copy .\questor\bin\debug\*.exe .\output\*.exe
 copy .\questor\bin\debug\*.dll .\output\*.dll
 copy .\gotobm\bin\debug\*.exe .\output\*.exe
 copy .\questorstatistics\bin\debug\*.exe .\output\*.exe
-copy .\questorsettings\bin\debug\*.exe .\output\*.exe
 copy .\updateinvtypes\bin\debug\*.exe .\output\*.exe
 copy .\valuedump\bin\debug\*.exe .\output\*.exe
+copy .\BuyLPI\bin\debug\*.exe .\output\*.exe
+copy .\Traveler\bin\debug\*.exe .\output\*.exe
+copy .\QuestorSettings\bin\debug\*.exe .\output\*.exe
 pause
