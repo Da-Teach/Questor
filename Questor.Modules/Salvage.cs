7c56fc6d550e922a74babceade98b24a6c3f5761
ISeeDEDPpl <bgates256@gmail.com>
2012-02-17 20:23:25 -0600

    Fix the openwreck code to only kick in when speedtank = true
    this allows the golem and other non-speed tanks to salvage/loot as they go
    looteverything=false does NOT mean do not loot anything
    it means loot aonly what you can loot without slowing me down

--- Questor.Modules/Salvage.cs
+++ Questor.Modules/Salvage.cs
@@ -448,7 +448,7 @@ namespace Questor.Modules
             }
 
             // Open a container in range
-            foreach (var containerEntity in Cache.Instance.Containers.Where(e => e.Distance <= 2500))
+            foreach (var containerEntity in Cache.Instance.Containers.Where(e => e.Distance <= 2400))
             {
                 // Emptry wreck, ignore
                 if (containerEntity.GroupId == (int) Group.Wreck && containerEntity.IsWreckEmpty)
@@ -467,8 +467,15 @@ namespace Questor.Modules
                 if (_openedContainers.ContainsKey(containerEntity.Id) && DateTime.Now.Subtract(_openedContainers[containerEntity.Id]).TotalSeconds < 10)
                     continue;
 
-                if (Settings.Instance.LootEverything == false && Cache.Instance.OpenWrecks == false)
+                // Don't even try to open a wreck if you are speed tanking and you aren't processing a loot action
+                if (Settings.Instance.SpeedTank == true && Cache.Instance.OpenWrecks == false)
                     continue;
+                
+                // Don't even try to open a wreck if you are specified LootEverything as false and you aren't processing a loot action
+                //      this is currently commented out as it would keep golems and other non-speed tanked ships from looting the field as they cleared
+                //      missions, but NOT stick around after killing things to clear it ALL. Looteverything==false does NOT mean loot nothing
+                //if (Settings.Instance.LootEverything == false && Cache.Instance.OpenWrecks == false)
+                //    continue;
 
                 // Open the container
                 Logging.Log("Salvage: Opening container [" + containerEntity.Name + "][" + containerEntity.Id + "]");
