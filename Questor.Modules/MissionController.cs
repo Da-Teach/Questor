59e7c0f75d4014453bc95e6714cb959b270fc68c
ISeeDEDPpl <bgates256@gmail.com>
2012-02-21 18:42:45 -0600

    Don't reload weapons that are busy doing other stuff m'kay?
    thank PluckyDuck for pointing it out...

--- Questor.Modules/MissionController.cs
+++ Questor.Modules/MissionController.cs
@@ -120,6 +120,9 @@ namespace Questor.Modules
                 if (weapon.CurrentCharges >= weapon.MaxCharges)
                     return;
 
+                if (weapon.IsReloadingAmmo || weapon.IsDeactivating || weapon.IsChangingAmmo)
+                    return;
+
                 if (_lastWeaponReload.ContainsKey(weapon.ItemId) && DateTime.Now < _lastWeaponReload[weapon.ItemId].AddSeconds(22))
                     return;
                 _lastWeaponReload[weapon.ItemId] = DateTime.Now;
