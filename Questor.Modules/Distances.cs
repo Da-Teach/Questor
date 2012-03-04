1a94d8ed055c07008d78c1c77b9d9fdc8311858b
ISeeDEDPpl <bgates256@gmail.com>
2012-03-04 04:29:49 -0600

    Adjusted some of the distances used by questor,  a slight adjustment to the Scooprange, an additional item "CloseToGateActivationRange" = GateActivationRange + 5000m
    InsideThisRangeIsLIkelyToBeMostlyFrigates has been adjusted down to a more reasonable  9000m

--- Questor.Modules/Distances.cs
+++ Questor.Modules/Distances.cs
@@ -11,16 +11,17 @@ namespace Questor.Modules
 {
     public enum Distance
     {
-        ScoopRange = 2500,
+        ScoopRange = 2490,
         SafeScoopRange = ScoopRange - 700,
         TooCloseToStructure = 3000,
         SafeDistancefromStructure = 5000,
         WarptoDistance = 152000,
         NextPocketDistance = 100000, // If we moved more then 100km, assume next Pocket
         GateActivationRange = 1000,
+        CloseToGateActivationRange = GateActivationRange + 5000,
         WayTooClose = -10100, // This is uaually used to determine how far inside the 'docking ring' of an acceleration gate we are. 
         OrbitDistanceCushion = 5000, // This is used to determine when to stop orbitingor approaching, if not speed tanking (orbit distance + orbitdistancecushion)
-        InsideThisRangeIsLIkelyToBeMostlyFrigates = 12000, // 12k - overall this assumption works, but there are missions where BSs will be at this range, use with caution
+        InsideThisRangeIsLIkelyToBeMostlyFrigates = 9000, // 9k - overall this assumption works, use with caution
         DecloakRange = 1900,
         SafeToCloakDistance = 2300,
         DockingRange = 0,
