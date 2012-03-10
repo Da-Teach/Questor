b749c2a77feb7f03f0c63249c7502ac9bb9e79d1
ISeeDEDPpl <bgates256@gmail.com>
2012-03-09 22:48:51 -0600

    Cleanup of Transfer to Live Copy

--- #TransferToLiveCopy#.bat
+++ #TransferToLiveCopy#.bat
@@ -3,5 +3,17 @@ set innerspacedotnetdirectory="..\..\innerspace\.net programs\"
 
 copy /y .\output\*.exe %innerspacedotnetdirectory% 
 copy /y .\output\*.dll %innerspacedotnetdirectory% 
-copy /y .\output\*.xml %innerspacedotnetdirectory%  
+::
+:: only copy invtypes.xml if one does not already exist (it contains pricing data)
+if not exist %innerspacedotnetdirectory%invtypes.xml copy /y .\output\invtypes.xml %innerspacedotnetdirectory%
+::
+:: always copy the template settings.xml file to dest
+copy /y .\output\settings.xml %innerspacedotnetdirectory%
+::
+::only copy ShipTargetValues.xml if one does not already exist (it contains targeting data)
+if not exist %innerspacedotnetdirectory%ShipTargetValues.xml copy /y .\output\ShipTargetValues.xml %innerspacedotnetdirectory%  
+::
+:: always copy factions.xml
+copy /y .\output\factions.xml %innerspacedotnetdirectory%
+
 pause
